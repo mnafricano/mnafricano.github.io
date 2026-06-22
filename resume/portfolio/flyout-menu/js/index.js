7c98c51d85b13307dcad0af89c96953fc0a72b13
Marcello Africano <mnafricano@users.noreply.github.com>
2026-06-21 23:45:02 -0500

    Refactor flyout menu functionality to use vanilla JavaScript, improving performance and maintainability

--- resume/portfolio/flyout-menu/js/index.js
+++ resume/portfolio/flyout-menu/js/index.js
@@ -1,14 +1,26 @@
-(function() {
-  $(".flyout-btn").click(function() {
-    $(".flyout-btn").toggleClass("btn-rotate");
-    $(".flyout").find("a").removeClass();
-    return $(".flyout").removeClass("flyout-init fade").toggleClass("expand");
+(() => {
+  const button = document.querySelector('.flyout-btn');
+  const flyout = document.querySelector('.flyout');
+
+  if (!button || !flyout) return;
+
+  const items = flyout.querySelectorAll('a');
+
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+    button.classList.toggle('btn-rotate');
+    items.forEach((item) => item.className = '');
+    flyout.classList.remove('flyout-init', 'fade');
+    flyout.classList.toggle('expand');
   });
 
-  $(".flyout").find("a").click(function() {
-    $(".flyout-btn").toggleClass("btn-rotate");
-    $(".flyout").removeClass("expand").addClass("fade");
-    return $(this).addClass("clicked");
+  items.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      event.preventDefault();
+      button.classList.toggle('btn-rotate');
+      flyout.classList.remove('expand');
+      flyout.classList.add('fade');
+      item.classList.add('clicked');
+    });
   });
-
-}).call(this);
+})();
